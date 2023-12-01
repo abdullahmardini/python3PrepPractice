fc398ba6ac1be6397a72a29c93fea3f3834b92a9
Abdullah Mardini <abmardini1@outlook.com>
2023-11-30 20:15:01 -0800

    add some test cases. hopefully that make sense

--- test_distributed_unique_id.py
+++ test_distributed_unique_id.py
@@ -5,6 +5,17 @@ test_disributed_unique_id.py
 import pytest
 import distributed_unique_id as uid
 
+def test_uid_generator():
+    '''
+    test that time is actually going up
+    '''
+    uid1 = uid.unique_key_generator()
+    uid2 = uid.unique_key_generator()
+    uid3 = uid.unique_key_generator()
+
+    assert uid1 < uid2
+    assert uid2 < uid3
+
 def test_get_machine_id():
     '''
     test machine id function
@@ -13,21 +24,22 @@ def test_get_machine_id():
     assert isinstance(machine_id, str)
     machine_id_2 = uid.get_machine_id()
     assert machine_id == machine_id_2
-    # Additional assertions can be added to validate the format of the machine ID
 
 def test_get_counter_increments():
     '''
     test counter, ensure it increments
     '''
-    initial_count = uid.get_counter()
-    next_count = uid.get_counter()
+    generator = uid.CounterGenerator()
+    initial_count = generator.get_counter(100)
+    next_count = generator.get_counter(100)
     assert next_count == initial_count + 1
 
-@pytest.mark.parametrize("execution_number", range(5))
-def test_get_counter_uniqueness(execution_number):
+def test_get_counter_restarts():
     '''
-    test counter, ensure uniqueness
+    test counter, ensure it restarts on different time
     '''
-    # This will run the test 5 times to ensure the counter increments uniquely each time
-    count = uid.get_counter()
-    assert count == execution_number + 1
+    generator = uid.CounterGenerator()
+    generator.get_counter(100)
+    next_count = generator.get_counter(101)
+    assert next_count == 0
+
