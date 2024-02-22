7aee5be65c498a7a92b46cd03cb40344d6a08b14
Abdullah Mardini <abmardini1@outlook.com>
2024-02-21 18:14:25 -0800

    I can't spell

--- test_shuffle.py
+++ test_shuffle.py
@@ -8,8 +8,8 @@ def test_shuffle_recursive():
     '''
     input: none
     return: none
-    test the recursive varient
-    Just a bunch of basic tests to make sure my code has a sembelence of sanity
+    test the recursive variant
+    Just a bunch of basic tests to make sure my code has a semblance of sanity
     '''
     string = 'abcd'
     shuffled = shuffle.shuffle_string_recursive(string)
@@ -21,8 +21,8 @@ def test_shuffle_loop():
     '''
     input: none
     return: none
-    test the iterative varient
-    Just a bunch of basic tests to make sure my code has a sembelence of sanity
+    test the iterative variant
+    Just a bunch of basic tests to make sure my code has a semblance of sanity
     '''
     string = 'abcd'
     shuffled = shuffle.shuffle_string_loop(string)
